53f26a67594d62eac11f026512ee7aa77555184f
salomaozao <salomao.bertolucci@gmail.com>
2020-12-24 00:10:16 -0300

    .

--- pages/cadastro/index.js
+++ pages/cadastro/index.js
@@ -37,7 +37,7 @@ const cadastro = (props) => {
 				{
 					height: useWindowDimensions().height,
 					flexDirection: "column",
-					justifyContent: "space-between",
+					justifyContent: 'space-between',
 				})
 			}
 		>

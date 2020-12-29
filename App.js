83a8dc77a7de8bfd430be2eb512e27d2ca0b4296
salomaozao <salomao.bertolucci@gmail.com>
2020-12-29 01:16:17 -0300

    REACT NATIVE SETUP

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+import { TextInput } from "react-native-paper"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>ahahaha</Text>
+			<TextInput></TextInput>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+})

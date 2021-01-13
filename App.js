7876d90ec0419e0290dc09c723a7973b32df693f
salomaozao <salomao.bertolucci@gmail.com>
2021-01-13 01:32:35 -0300

    Start

--- App.js
+++ App.js
@@ -1,130 +1,47 @@
-// import React, { Component } from "react"
-// import { DefaultTheme, Text, PaperProvider, Colors } from "react-native-paper"
-// import RouterProvider from "@react-navigation/native"
+import * as React from "react"
+import "react-native-gesture-handler"
+import {
+	DefaultTheme,
+	Text,
+	Provider as PaperProvider,
+	Colors,
+	Button,
+	BottomNavigation,
+} from "react-native-paper"
 
-// import PaperNav from "./pages/generalComponents/navigation"
-
-// import Agendamentos from "./pages/agendamentos/index"
-// import Cadastro from "./pages/cadastro/index"
-// import ProductView from "./pages/productExibition"
-// import Acc from "./pages/acc/index"
-
-// const theme = {
-// 	...DefaultTheme,
-// 	roundness: 2,
-// 	colors: {
-// 		...DefaultTheme.colors,
-// 		primary: Colors.blue900,
-// 		accent: "#f1c40f",
-// 		// background: Colors.blueA100,
-// 		surface: Colors.grey100,
-// 		text: Colors.grey700,
-// 	},
-// }
-
-// export default class App extends Component {
-// 	// componentDidMount() {
-// 	// 	Orientation.getInitialOrientation()
-// 	// }
-
-// 	render() {
-// 		return (
-// 			<Provider theme={theme}>
-// 				<RouterProvider>
-// 					<Nav
-// 						scenes={[
-// 							<Agendamentos />,
-// 							<ProductView />,
-// 							<Cadastro />,
-// 						]}
-// 					/>
-// 				</RouterProvider>
-// 			</Provider>
-// 		)
-// 	}
-// }
-
-import React, { useState } from "react"
-import { View, Text } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
-import { Button, TextInput } from "react-native-paper"
+//pages
+import Agendamentos from "./pages/agendamentos/index"
+import Cadastro from "./pages/cadastro/index"
+import ProductView from "./pages/productExibition"
+import Acc from "./pages/acc/index"
+
+const theme = {
+	...DefaultTheme,
+	roundness: 2,
+	colors: {
+		...DefaultTheme.colors,
+		primary: Colors.blue900,
+		accent: "#f1c40f",
+		// background: Colors.blueA100,
+		surface: Colors.grey100,
+		text: Colors.grey700,
+	},
+}
+
 const Stack = createStackNavigator()
 
 const App = () => {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator
-				screenOptions={{
-					headerRight: () => (
-						<Button
-							onPress={() => alert("This is a button!")}
-							title="Info"
-							color="#fff"
-						/>
-					),
-				}}
-				initialRouteName="Page1"
-			>
-				<Stack.Screen
-					name="home"
-					options={{
-						title: "Casa",
-						headerStyle: { backgroundColor: "green" },
-					}}
-					props="Gabriel"
-				>
-					{(props) => <Home {...props} />}
-				</Stack.Screen>
-				<Stack.Screen
-					name="Page1"
-					component={Page1}
-					initialParams={{ pw: "haha inicio" }}
-					options={(routes) => routes.un}
-				/>
-			</Stack.Navigator>
+			<PaperProvider theme={theme}>
+				<Stack.Navigator>
+					<Stack.Screen name="acc" component={Acc} />
+				</Stack.Navigator>
+				<Button>a</Button>
+			</PaperProvider>
 		</NavigationContainer>
-	)
-}
-
-const Home = ({ navigation, route }) => {
-	return (
-		<View style={{ justifyContent: "center", alignItems: "center" }}>
-			<Text>Olá, {route.params.username}! Eu sou a página principal</Text>
-			<Button
-				onPress={() =>
-					navigation.navigate("Page1", { pw: "1234", un: "João" })
-				}
-			>
-				passar parâmetro
-			</Button>
-
-			<Button onPress={() => navigation.goBack({ pw: "1010" })}>
-				Voltar
-			</Button>
-		</View>
-	)
-}
-
-const Page1 = ({ navigation, route }) => {
-	const [inptVal, setVal] = useState("neymar")
-	return (
-		<View>
-			<Text>Sou a página 2!</Text>
-			<Text>senha: {route.params.pw}</Text>
-			<TextInput
-				onChangeText={(text) => navigation.setOptions({ title: text })}
-			/>
-			<Button
-				onPress={() => {
-					navigation.navigate("home", {
-						username: inptVal,
-					})
-				}}
-			>
-				Ir pra home, parâmetro: {"<TextInput>"}
-			</Button>
-		</View>
 	)
 }
 
